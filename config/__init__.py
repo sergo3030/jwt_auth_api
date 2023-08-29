7465d145f8d5622bc78da589293f3ed35c830f79
unknown <seriy2000@protonmail.com>
2023-08-29 15:11:51 +0300

    configuration: fixing issue of 'no such file or diractory' for config file while importing from nested directory

--- config/__init__.py
+++ config/__init__.py
@@ -2,8 +2,9 @@ from envyaml import EnvYAML
 import os
 
 
-environment = os.getenv('ENV_VAR')
-if environment is None:
-    environment = "dev"
-config_file_name = f"config_{environment}.yaml"
-config = EnvYAML(yaml_file=f"config/{config_file_name}")
+env = os.getenv('ENV_VAR')
+if env is None:
+    env = "dev"
+
+config_file = f"config_{env}.yaml"
+config = EnvYAML(yaml_file=f"{os.path.dirname(__file__)}/{config_file}")
